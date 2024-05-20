0c487ac7883558d0684078d6a63a4fff264b7cf6
Brayan Guillermo Diaz Martinez <brayan.diaz@clinicasanluis.com.co>
2024-05-20 07:56:50 -0500

    fix: :fire: rename a file not found

---
+++ src/modules/user/layout/userLayout.vue
@@ -0,0 +1,10 @@
+<script setup>
+import { defineAsyncComponent } from 'vue'
+const layoutComponent = defineAsyncComponent(() => import('@s/components/dashboardViews.vue'))
+</script>
+
+<template>
+  <layout-component>
+    <router-view />
+  </layout-component>
+</template>
